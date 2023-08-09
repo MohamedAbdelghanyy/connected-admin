9384e585b71f09a67fee43572f0212df32808906
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2023-08-09 12:41:28 +0300

    Added Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React, { Component, Suspense } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CSpinner } from '@coreui/react-pro'
 import './scss/style.scss'
 
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+const Login = React.lazy(() => import('./views/login/Login'))
 
 class App extends Component {
   render(): JSX.Element {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <Suspense fallback={<CSpinner color="primary" />}>
           <Routes>
             <Route path="*" element={<DefaultLayout />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </Suspense>
-      </HashRouter>
+      </BrowserRouter>
     )
   }
 }
